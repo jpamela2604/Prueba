843ad8b439b203c662048becda72122773fc401b
Pamela <Pamela@PAM>
2017-09-12 22:51:27 -0600

    Primera Construccion

---
+++ mavenproject1/src/test/java/WSETEST/WSTEST.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WSETEST;
+
+import SWE.SW;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pamela
+ */
+public class WSTEST {
+    SW request = new SW();
+    public WSTEST() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+     @Test
+    public void prueba()
+    {
+        String r= this.request.hello("hola");
+        assertNotEquals(r,"");
+    }
+}
